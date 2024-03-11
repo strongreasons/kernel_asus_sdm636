43d646769221244487ecca1571298166839a8a2e
strongreasons <abenkenari@gmail.com>
2024-03-11 14:02:51 +0000

    rearrange

--- regen.sh
+++
@@ -1,1 +0,0 @@
-export ARCH=arm64 && export SUBARCH=arm64 && make X00TD_defconfig && mv .config arch/arm64/configs/X00TD_defconfig && git add arch/arm64/configs/X00TD_defconfig && git commit -m "defconfig: Regen" -s
